92e3cb96895a3f7ecc6a23d597d76312219304ca
Alcenor Costa de Almeida <alcenor.almeida@tjse.jus.br>
2019-02-28 13:47:19 -0300

    Ajustes para autenticação usando "basic-security"

--- src/main/java/com/example/algamoney/api/config/AuthenticationMananagerProvider.java
+++ src/main/java/com/example/algamoney/api/config/AuthenticationMananagerProvider.java
@@ -2,10 +2,12 @@ package com.example.algamoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Order(200)
 public class AuthenticationMananagerProvider extends WebSecurityConfigurerAdapter {
 
 	@Bean
